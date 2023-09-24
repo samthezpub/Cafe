41ce8d4c7d4e0085f72ae1eec17ae951f2179de9
Владимир <lil.rhymes9@gmail.com>
2023-09-24 19:54:20 +0300

    Staff class added, PositionsEnum for position and role in cafe

---
+++ src/main/java/org/example/Enums/PositionsEnum.java
@@ -0,0 +1,8 @@
+package org.example.Enums;
+
+public enum PositionsEnum {
+    BARISTA,
+    WAITER,
+    CONFECTIONER,
+
+}
---
+++ src/main/java/org/example/Staff.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.Enums.PositionsEnum;
+
+public class Staff {
+    private String name;
+    private String phone;
+    private String email;
+    private PositionsEnum position;
+}
